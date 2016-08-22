48b03154708c29935df08dd208702d38b24aaed5
Travis Clark <trclark@MACCINTRCLARK.local>
2016-08-22 16:32:46 -0400

    moved specialevents code to new file, moved all files into root dir

---
+++ SpecialEvents.js
@@ -0,0 +1,10 @@
+/* SpecialEvents.js */
+/**
+ * This file contains callback functions for special triggered events on cetrain loading screens
+**/
+
+/* sunny() plays*/
+function sunny(){
+	var audio = new Audio('res/sunny.mp3');
+	audio.play();
+};
