64c368146da962d20cdf604f024754e8b8703d4e
trclark-progress <travis.clark@progress.com>
2016-09-08 14:23:19 -0400

    refactored SpecialEvents into it's own namespace

--- Directory.js
+++ Directory.js
@@ -12,90 +12,90 @@
 
 var loadingScreenDirectory = [
 	{
-		url:"http://i.imgur.com/J0zYUX2.png",
-		title:"The Original",
-        credit:"Brett"
+		url: "http://i.imgur.com/J0zYUX2.png",
+		title: "The Original",
+        credit: "Brett"
 	},
     {
-		url:"http://i.imgur.com/sDYG7yd.png",
-		title:"Sweet Skins and Hella Mods",
-        credit:"Rav-T"
+		url: "http://i.imgur.com/sDYG7yd.png",
+		title: "Sweet Skins and Hella Mods",
+        credit: "Rav-T"
 	},
     {
-		url:"http://i.imgur.com/lmMNbwD.png",
-		title:"MLG",
-        credit:"Mike"
+		url: "http://i.imgur.com/lmMNbwD.png",
+		title: "MLG",
+        credit: "Mike"
 	},
     {
-		url:"http://i.imgur.com/7q6bIB0.png",
-		title:"/u/",
-        credit:"Rav-T"
+		url: "http://i.imgur.com/7q6bIB0.png",
+		title: "/u/",
+        credit: "Rav-T"
 	},
     {
-		url:"http://i.imgur.com/Zex9uqR.png",
-		title:"Terrorist Town Gazette 01-01",
-        credit:"Rav-T"
+		url: "http://i.imgur.com/Zex9uqR.png",
+		title: "Terrorist Town Gazette 01-01",
+        credit: "Rav-T"
 	},
     {
-		url:"http://i.imgur.com/AmDjSaW.jpg",
-		title:"Safety Record",
-        credit:"Brett"
+		url: "http://i.imgur.com/AmDjSaW.jpg",
+		title: "Safety Record",
+        credit: "Brett"
 	},
     {
-		url:"http://i.imgur.com/qkV5qT9.jpg",
-		title:"Atlas Park",
-        credit:"Matt"
+		url: "http://i.imgur.com/qkV5qT9.jpg",
+		title: "Atlas Park",
+        credit: "Matt"
 	},
     {
-		url:"http://i.imgur.com/rP04dbU.jpg",
-		title:"Winners",
-        credit:"Mike, Rav-T"
+		url: "http://i.imgur.com/rP04dbU.jpg",
+		title: "Winners",
+        credit: "Mike, Rav-T"
 	},
     {
-		url:"http://i.imgur.com/FgbK3qI.jpg",
-		title:"Damage Control",
-        credit:"Rav-T"
+		url: "http://i.imgur.com/FgbK3qI.jpg",
+		title: "Damage Control",
+        credit: "Rav-T"
 	},
     {
-		url:"http://i.imgur.com/vvmlkuv.png",
-		title:"Postcards",
-        credit:"Rav-T"
+		url: "http://i.imgur.com/vvmlkuv.png",
+		title: "Postcards",
+        credit: "Rav-T"
 	},
     {
-		url:"http://i.imgur.com/qPkc4CV.png",
-		title:"Robotnik",
-        credit:"Rav-T"
+		url: "http://i.imgur.com/qPkc4CV.png",
+		title: "Robotnik",
+        credit: "Rav-T"
 	},
     {
-		url:"http://i.imgur.com/yFbdPTn.png",
-		title:"Don't Forget:",
-        credit:"Rav-T"
+		url: "http://i.imgur.com/yFbdPTn.png",
+		title: "Don't Forget:",
+        credit: "Rav-T"
 	},
     {
-		url:"http://i.imgur.com/lrmn5ek.png",
-		title:"Badmins",
-        credit:"Jason"
+		url: "http://i.imgur.com/lrmn5ek.png",
+		title: "Badmins",
+        credit: "Jason"
 	},
     {
-		url:"http://i.imgur.com/pMCeOiw.png",
-		title:"Minimal",
-        credit:"Mike, Rav-T"
+		url: "http://i.imgur.com/pMCeOiw.png",
+		title: "Minimal",
+        credit: "Mike, Rav-T"
 	},
     {
-		url:"http://i.imgur.com/3bBK6Fc.jpg",
-		title:"No Fun",
-        credit:"Brett"
+		url: "http://i.imgur.com/3bBK6Fc.jpg",
+		title: "No Fun",
+        credit: "Brett"
 	},
     {
-		url:"http://i.imgur.com/0RDXGqs.jpg",
-		title:"Always Sunny in Terrortown",
-        credit:"Brett",
-        trigger:sunny
+		url: "http://i.imgur.com/0RDXGqs.jpg",
+		title: "Always Sunny in Terrortown",
+        credit: "Brett",
+        trigger: SpecialEvents.sunny
     },
 	{
-		url:"http://i.imgur.com/XUVcFQx.jpg",
+		url: "http://i.imgur.com/XUVcFQx.jpg",
 		title: "ERROR",
 		credit: "Rav-T",
-		trigger: indexError
+		trigger: SpecialEvents.indexError
 	}
 ];
--- SpecialEvents.js
+++ SpecialEvents.js
@@ -4,48 +4,48 @@
 **/
 
 //~~~~~~~~~~~~UTILITY FUNCTIONS~~~~~~~~~~~~
+var SpecialEvents = new (function(){
+	/**
+	 * Generic function that plays a sound located at the indicated path
+	**/
+	function playsound(path){
+		var sfx = document.createElement("audio");
+		sfx.src = path;
+		sfx.volume = 0.3;
+		document.getElementById("logo").appendChild(sfx);
+		sfx.play();
+	}
 
-/**
- * Generic function that plays a sound located at the indicated path
-**/
-function playsound(path){
-	var sfx = document.createElement("audio");
-	sfx.src = path;
-	sfx.volume = 0.3;
-	document.getElementById("logo").appendChild(sfx);
-	sfx.play();
-}
+	//~~~~~~~~~~~~EVENT FUNCTIONS~~~~~~~~~~~~
 
-//~~~~~~~~~~~~EVENT FUNCTIONS~~~~~~~~~~~~
-
-/** sunny()
- * Plays the theme from "Always Sunny In Philadelphia"
- * Trigger Screen: 'Always Sunny in Terrortown' by Brett
-**/
-function sunny(){
-	playsound("res/audio/sunny.mp3");
-};
+	/** sunny()
+	 * Plays the theme from "Always Sunny In Philadelphia"
+	 * Trigger Screen: 'Always Sunny in Terrortown' by Brett
+	**/
+	this.sunny = function(){
+		playsound("res/audio/sunny.mp3");
+	};
 
 
-/** indexError()
- * Fakes a "Too many indices" error by clearing out the normal display elements
- * and playing a windows XP error sound. Also runs feature detection with
- * output visible on the loading screen for future programmer use. For testing,
- * a mouseclick on the screen will bring back the display elements.
- * Tell a programmer!
-**/
-function indexError(){
-
-	document.addEventListener(
-		"click",
-		function clickToReturn(){
-			document.getElementById("boilerplate").style.display = "initial";
-			document.getElementById("logo").style.display = "initial";
-			document.getElementById("test").style.display = "initial";
-			document.removeEventListener("click", clickToReturn, false);
-		}, false);
-	document.getElementById("boilerplate").style.display = 'none';
-	document.getElementById("logo").style.display = "none";
-	document.getElementById("test").style.display = "none";
-	playsound('res/audio/error-ding.wav');
-}
+	/** indexError()
+	 * Fakes a "Too many indices" error by clearing out the normal display elements
+	 * and playing a windows XP error sound. Also runs feature detection with
+	 * output visible on the loading screen for future programmer use. For testing,
+	 * a mouseclick on the screen will bring back the display elements.
+	 * Tell a programmer!
+	**/
+	this.indexError = function(){
+		document.addEventListener(
+			"click",
+			function clickToReturn(){
+				document.getElementById("boilerplate").style.display = "initial";
+				document.getElementById("logo").style.display = "initial";
+				document.getElementById("test").style.display = "initial";
+				document.removeEventListener("click", clickToReturn, false);
+			}, false);
+		document.getElementById("boilerplate").style.display = 'none';
+		document.getElementById("logo").style.display = "none";
+		document.getElementById("test").style.display = "none";
+		playsound('res/audio/error-ding.wav');
+	}
+})();
