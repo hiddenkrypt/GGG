07c693e8176a58bde0807999bcece9050d32af57
trclark-progress <travis.clark@progress.com>
2016-09-13 12:09:30 -0400

    Moved hideUI logic into it's own utility function

--- SpecialEvents.js
+++ SpecialEvents.js
@@ -4,6 +4,8 @@
 **/
 
 var SpecialEvents = new (function(){
+
+//~~~~~~~~~~~~UTILITY FUNCTIONS~~~~~~~~~~~~
 	/**
 	 * Generic function that plays a sound located at the indicated path
 	**/
@@ -14,26 +16,11 @@ var SpecialEvents = new (function(){
 		document.getElementById("logo").appendChild(sfx);
 		sfx.play();
 	}
-
-	//~~~~~~~~~~~~EVENT FUNCTIONS~~~~~~~~~~~~
-
-	/** sunny()
-	 * Plays the theme from "Always Sunny In Philadelphia"
-	 * Trigger Screen: 'Always Sunny in Terrortown' by Brett
+	/**
+	 * Hides the DOM Elements normally on the page, and sets up a clickhandler
+	 * that will bring the elements back
 	**/
-	this.sunny = function(){
-		playsound("res/audio/sunny.wav");
-	};
-
-
-	/** indexError()
-	 * Fakes a "Too many indices" error by clearing out the normal display elements
-	 * and playing a windows XP error sound. Also runs feature detection with
-	 * output visible on the loading screen for future programmer use. For testing,
-	 * a mouseclick on the screen will bring back the display elements.
-	 * Tell a programmer!
-	**/
-	this.indexError = function(){
+	function hideUI(){
 		document.addEventListener(
 			"click",
 			function clickToReturn(){
@@ -45,6 +32,32 @@ var SpecialEvents = new (function(){
 		document.getElementById("boilerplate").style.display = 'none';
 		document.getElementById("logo").style.display = "none";
 		document.getElementById("test").style.display = "none";
+	}
+
+
+
+
+//~~~~~~~~~~~~EVENT FUNCTIONS~~~~~~~~~~~~
+
+	/** sunny()
+	 * Plays the theme from "Always Sunny In Philadelphia"
+	 * Trigger Screen: 'Always Sunny in Terrortown' by Brett
+	**/
+	this.sunny = function(){
+		playsound("res/audio/sunny.wav");
+	};
+
+
+	/** indexError()
+	 * Fakes a "Too many indices" error by clearing out the normal display
+	 * elements and playing a windows XP error sound. For testing, a mouseclick
+	 * on the screen will bring back the display elements.
+	 * Tell a programmer!
+	**/
+	this.indexError = function(){
+		hideUI();
 		playsound('res/audio/error-ding.wav');
 	}
+
+
 })();
