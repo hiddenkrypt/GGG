2207d71b2188d04852f58f86045169fe4900317e
Rav-T <hiddenkrypt@gmail.com>
2019-01-24 17:33:53 -0800

    switching to archived JSON of the cah decks, rather than live grabbign them

--- SpecialEvents.js
+++ SpecialEvents.js
@@ -1,3 +1,4 @@
+
 /** SpecialEvents.js **
  * This file contains callback functions for special triggered events on some
  * loading screens. Should be referenced and loaded before Directoy.js
@@ -119,119 +120,100 @@ var SpecialEvents = new (function(){
 		})()
 	}
   this.spooky = function(){
-    document.body.style.background = "#000"
-    var C_WIDTH = 501
-    var C_HEIGHT = 687
-    var girlLoaded = false
-    var spookLoaded = false
-    var ctx = addCanvas(C_WIDTH,C_HEIGHT)
-    var girl = new Image(501, 687)
-    var spook = new Image(400, 358)
-    var cutOff = 456
-    var cut = 0
-    girl.onload = function(){girlLoaded = true}
-    spook.onload = function(){spookLoaded = true}
-    girl.src = 'res/img/girl.jpg'
-    spook.src = 'res/img/spooky.jpg'
+    document.body.style.background = "#000";
+    var C_WIDTH = 501;
+    var C_HEIGHT = 687;
+    var girlLoaded = false;
+    var spookLoaded = false;
+    var ctx = addCanvas(C_WIDTH,C_HEIGHT);
+    var girl = new Image(501, 687);
+    var spook = new Image(400, 358);
+    var cutOff = 456;
+    var cut = 0;
+    girl.onload = function(){girlLoaded = true;};
+    spook.onload = function(){spookLoaded = true;};
+    girl.src = 'res/img/girl.jpg';
+    spook.src = 'res/img/spooky.jpg';
     function spookem(){
       if(girlLoaded){
-        ctx.drawImage(girl, 0, 0)
-        ctx.fillStyle = "#ffffff"
-        ctx.fillRect(0,cut,1000,1000)
+        ctx.drawImage(girl, 0, 0);
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(0,cut,1000,1000);
         if(cut < cutOff){
           cut+=1.25;
         }
       }
       if(spookLoaded && cut >= cutOff){
-        ctx.clearRect(0,0,C_WIDTH,C_HEIGHT)
-        ctx.drawImage(spook, (C_WIDTH - spook.width) /2,(C_HEIGHT - spook.height) /2)
-        playsound("res/audio/spooky.ogg", .06, false)
-        document.getElementById("logo").src = "res/img/doot.gif"
-        document.getElementById("title").innerHTML = "Gentlemen's SPOOK GROUP"
-        var meter = document.createElement("img")
-        var doot2 = document.createElement("img")
-        meter.src = "res/img/2SpookyMeter.gif"
-        doot2.src = "res/img/doot2.gif"
-        meter.style.float = "left"
-        doot2.style.float = "right"
-        setTimeout(function(){ document.body.appendChild(doot2)}, 3300)
-        setTimeout(function(){ document.body.appendChild(doot2.cloneNode())}, 4300)
-        setTimeout(function(){ document.body.appendChild(doot2.cloneNode())}, 5000)
-        setTimeout(function(){ document.body.appendChild(doot2.cloneNode())}, 6000)
-        setTimeout(function(){ document.body.appendChild(doot2.cloneNode())}, 6300)
-        setTimeout(function(){ document.body.appendChild(doot2.cloneNode())}, 6700)
-        setTimeout(function(){ document.body.appendChild(doot2.cloneNode())}, 6800)
-        setTimeout(function(){ document.body.appendChild(doot2.cloneNode())}, 6900)
-        setTimeout(function(){ document.body.appendChild(doot2.cloneNode())}, 6950)
-        setTimeout(function(){ document.body.appendChild(doot2.cloneNode())}, 7000)
-        setTimeout(function(){ document.body.appendChild(meter)}, 1600)
+        ctx.clearRect(0,0,C_WIDTH,C_HEIGHT);
+        ctx.drawImage(spook, (C_WIDTH - spook.width) /2,(C_HEIGHT - spook.height) /2);
+        playsound("res/audio/spooky.ogg", 0.06, false);
+        document.getElementById("logo").src = "res/img/doot.gif";
+        document.getElementById("title").innerHTML = "Gentlemen's SPOOK GROUP";
+        var meter = document.createElement("img");
+        var doot2 = document.createElement("img");
+        meter.src = "res/img/2SpookyMeter.gif";
+        doot2.src = "res/img/doot2.gif";
+        meter.style.float = "left";
+        doot2.style.float = "right";
+        setTimeout(function(){ document.body.appendChild(doot2);}, 3300);
+        setTimeout(function(){ document.body.appendChild(doot2.cloneNode());}, 4300);
+        setTimeout(function(){ document.body.appendChild(doot2.cloneNode());}, 5000);
+        setTimeout(function(){ document.body.appendChild(doot2.cloneNode());}, 6000);
+        setTimeout(function(){ document.body.appendChild(doot2.cloneNode());}, 6300);
+        setTimeout(function(){ document.body.appendChild(doot2.cloneNode());}, 6700);
+        setTimeout(function(){ document.body.appendChild(doot2.cloneNode());}, 6800);
+        setTimeout(function(){ document.body.appendChild(doot2.cloneNode());}, 6900);
+        setTimeout(function(){ document.body.appendChild(doot2.cloneNode());}, 6950);
+        setTimeout(function(){ document.body.appendChild(doot2.cloneNode());}, 7000);
+        setTimeout(function(){ document.body.appendChild(meter);}, 1600);
       } else {
-        anim(spookem)
+        anim(spookem);
       }
-    };
-    spookem()
+    }
+    spookem();
   };
   this.loadCards = function(){
-    function parseDeck(){
-      var cardSize = "250";
-      var deck = JSON.parse(this.responseText);
-      console.log(deck)
-      var cardBox = document.createElement("div");
+    var cardSize = "250";
+    var cardBox = document.createElement("div");
 
-      var grab = Math.floor(Math.random()*deck.calls.length)
-      var call = deck.calls[grab];
-      var blackCard = document.createElement("div");
-      blackCard.style.backgroundColor = "#000";
-      blackCard.style.display = "inline-block"
-      blackCard.style.width = cardSize+"px";
-      blackCard.style.height = cardSize+"px";
-      blackCard.style.borderRadius = ((cardSize*0.15)>>0)+"px";
-      blackCard.style.padding = ((cardSize*0.15)>>0)+"px";
-      blackCard.style.margin = ((cardSize*0.15)>>0)+"px";
-      blackCard.style.border = "1px solid white";
-      blackCard.style.boxShadow = ((cardSize*0.1)>>0)+"px " + ((cardSize*0.15)>>0)+"px #888888";
-      blackCard.style.color = "#fff";
-      blackCard.style.fontSize = ((cardSize/12)>>0)+"px";
-      blackCard.innerHTML = call.text.join(" ____ ");
-      cardBox.appendChild(blackCard);
-      function addWhite(){
-        var grabRes = Math.floor(Math.random()*deck.responses.length);
-        var resp = deck.responses[grabRes];
-        var whiteCard = document.createElement("div");
-        whiteCard.style.backgroundColor = "#fff";
-        whiteCard.style.display = "inline-block"
-        whiteCard.style.width = cardSize+"px";
-        whiteCard.style.height = cardSize+"px";
-        whiteCard.style.borderRadius = ((cardSize*0.15)>>0)+"px";
-        whiteCard.style.padding = ((cardSize*0.15)>>0)+"px";
-        whiteCard.style.margin = ((cardSize*0.15)>>0)+"px";
-        whiteCard.style.border = "1px solid black";
-        whiteCard.style.boxShadow = ((cardSize*0.1)>>0)+"px " + ((cardSize*0.15)>>0)+"px #888888";
-        whiteCard.style.color = "#000";
-        whiteCard.style.fontSize = ((cardSize/12)>>0)+"px";
-        whiteCard.innerHTML = resp.text[0];
-        cardBox.appendChild(whiteCard);
-      }
-      for(var i = 0; i < call.text.length-1; i++){
-        console.log("")
-        addWhite();
-      }
-
-      document.body.appendChild(cardBox);
-
+    var grab = Math.floor(Math.random()*deck.calls.length);
+    var call = deck.calls[grab];
+    var blackCard = document.createElement("div");
+    blackCard.style.backgroundColor = "#000";
+    blackCard.style.display = "inline-block";
+    blackCard.style.width = cardSize+"px";
+    blackCard.style.height = cardSize+"px";
+    blackCard.style.borderRadius = ((cardSize*0.15)>>0)+"px";
+    blackCard.style.padding = ((cardSize*0.15)>>0)+"px";
+    blackCard.style.margin = ((cardSize*0.15)>>0)+"px";
+    blackCard.style.border = "1px solid white";
+    blackCard.style.boxShadow = ((cardSize*0.1)>>0)+"px " + ((cardSize*0.15)>>0)+"px #888888";
+    blackCard.style.color = "#fff";
+    blackCard.style.fontSize = ((cardSize/12)>>0)+"px";
+    blackCard.innerHTML = call.text.join(" ____ ");
+    cardBox.appendChild(blackCard);
+    function addWhite(){
+      var grabRes = Math.floor(Math.random()*deck.responses.length);
+      var resp = deck.responses[grabRes];
+      var whiteCard = document.createElement("div");
+      whiteCard.style.backgroundColor = "#fff";
+      whiteCard.style.display = "inline-block";
+      whiteCard.style.width = cardSize+"px";
+      whiteCard.style.height = cardSize+"px";
+      whiteCard.style.borderRadius = ((cardSize*0.15)>>0)+"px";
+      whiteCard.style.padding = ((cardSize*0.15)>>0)+"px";
+      whiteCard.style.margin = ((cardSize*0.15)>>0)+"px";
+      whiteCard.style.border = "1px solid black";
+      whiteCard.style.boxShadow = ((cardSize*0.1)>>0)+"px " + ((cardSize*0.15)>>0)+"px #888888";
+      whiteCard.style.color = "#000";
+      whiteCard.style.fontSize = ((cardSize/12)>>0)+"px";
+      whiteCard.innerHTML = resp.text[0];
+      cardBox.appendChild(whiteCard);
     }
-    function failed(){
-      var errorBox = document.createElement("div");
-      errorBox.style.color="#ff0000";
-      errorBox.style.fontSize = "4em";
-      errorBox.innerHTML = "CAH CARD ERROR :<";
-      document.body.appendChild(errorBox);
+    for(var i = 0; i < call.text.length-1; i++){
+      addWhite();
     }
-    xmlhr = new XMLHttpRequest();
-    xmlhr.addEventListener("load", parseDeck);
-    xmlhr.addEventListener("error", failed);
-    xmlhr.open("GET", "https://api.cardcastgame.com/v1/decks/JPJVC/cards");
-    xmlhr.send();
-  }
+    document.body.appendChild(cardBox);
+  };
 
 })();
