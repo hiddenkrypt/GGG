de17511cb3778d216a31605ef6e6794f753e7822
Ada <hiddenkrypt@gmail.com>
2021-04-20 15:13:34 -0700

    deadname replace, formatting standardized

--- Directory.js
+++ Directory.js
@@ -8,184 +8,149 @@
  *	trigger: <function, optional> an optional callback function for
  *			 Special Events. This function should be in SpecialEvents.js
  * }
-**/
-var loadingScreenDirectory = [
-	{
-		url: "http://i.imgur.com/J0zYUX2.png",
-		title: "The Original",
+ **/
+var loadingScreenDirectory = [{
+        url: "http://i.imgur.com/J0zYUX2.png",
+        title: "The Original",
         credit: "Brett"
-	},
-    {
-		url: "http://i.imgur.com/sDYG7yd.png",
-		title: "Sweet Skins and Hella Mods",
-        credit: "Rav-T"
-	},
-    {
-		url: "http://i.imgur.com/lmMNbwD.png",
-		title: "MLG",
+    }, {
+        url: "http://i.imgur.com/sDYG7yd.png",
+        title: "Sweet Skins and Hella Mods",
+        credit: "Ada"
+    }, {
+        url: "http://i.imgur.com/lmMNbwD.png",
+        title: "MLG",
         credit: "Mike"
-	},
-    {
-		url: "http://i.imgur.com/7q6bIB0.png",
-		title: "/u/",
-        credit: "Rav-T"
-	},
-    {
-		url: "http://i.imgur.com/Zex9uqR.png",
-		title: "Terrorist Town Gazette 01-01",
-        credit: "Rav-T"
-	},
-    {
-		url: "http://i.imgur.com/AmDjSaW.jpg",
-		title: "Safety Record",
+    }, {
+        url: "http://i.imgur.com/7q6bIB0.png",
+        title: "/u/",
+        credit: "Ada"
+    }, {
+        url: "http://i.imgur.com/Zex9uqR.png",
+        title: "Terrorist Town Gazette 01-01",
+        credit: "Ada"
+    }, {
+        url: "http://i.imgur.com/AmDjSaW.jpg",
+        title: "Safety Record",
         credit: "Brett"
-	},
-    {
-		url: "http://i.imgur.com/qkV5qT9.jpg",
-		title: "Atlas Park",
+    }, {
+        url: "http://i.imgur.com/qkV5qT9.jpg",
+        title: "Atlas Park",
         credit: "Matt"
-	},
-    {
-		url: "http://i.imgur.com/rP04dbU.jpg",
-		title: "Winners",
-        credit: "Mike, Rav-T"
-	},
-    {
-		url: "http://i.imgur.com/FgbK3qI.jpg",
-		title: "Damage Control",
-        credit: "Rav-T"
-	},
-    {
-		url: "http://i.imgur.com/vvmlkuv.png",
-		title: "Postcards",
-        credit: "Rav-T"
-	},
-    {
-		url: "http://i.imgur.com/yFbdPTn.png",
-		title: "Don't Forget:",
-        credit: "Rav-T"
-	},
-    {
-		url: "http://i.imgur.com/lrmn5ek.png",
-		title: "Badmins",
+    }, {
+        url: "http://i.imgur.com/rP04dbU.jpg",
+        title: "Winners",
+        credit: "Mike, Ada"
+    }, {
+        url: "http://i.imgur.com/FgbK3qI.jpg",
+        title: "Damage Control",
+        credit: "Ada"
+    }, {
+        url: "http://i.imgur.com/vvmlkuv.png",
+        title: "Postcards",
+        credit: "Ada"
+    }, {
+        url: "http://i.imgur.com/yFbdPTn.png",
+        title: "Don't Forget:",
+        credit: "Ada"
+    }, {
+        url: "http://i.imgur.com/lrmn5ek.png",
+        title: "Badmins",
         credit: "Jason"
-	},
-    {
-		url: "http://i.imgur.com/pMCeOiw.png",
-		title: "Minimal",
-        credit: "Mike, Rav-T"
-	},
-    {
-		url: "http://i.imgur.com/3bBK6Fc.jpg",
-		title: "No Fun",
+    }, {
+        url: "http://i.imgur.com/pMCeOiw.png",
+        title: "Minimal",
+        credit: "Mike, Ada"
+    }, {
+        url: "http://i.imgur.com/3bBK6Fc.jpg",
+        title: "No Fun",
         credit: "Brett"
-	},
-    {
-		url: "http://i.imgur.com/0RDXGqs.jpg",
-		title: "Always Sunny in Terrortown",
+    }, {
+        url: "http://i.imgur.com/0RDXGqs.jpg",
+        title: "Always Sunny in Terrortown",
         credit: "Brett",
         trigger: SpecialEvents.sunny
-    },
-	{
-		url: "http://i.imgur.com/XUVcFQx.jpg",
-		title: "ERROR",
-		credit: "Rav-T",
-		trigger: SpecialEvents.indexError
-  },
-	{
-		url:"http://i.imgur.com/s65Yr86.png",
-		title:"components",
-		credit:"Rav-T, Ken"
-	},
-	{
-		url: "http://i.imgur.com/IEosSyF.png",
-		title: "SuperNERD",
-		credit: "Rav-T, Ken"
-	},
-	{
-		url: "http://i.imgur.com/CtRAxUR.png",
-		title: "Stranger Salts",
-		credit: "Rav-T"
-	},
-	{
-		url: "http://i.imgur.com/5qeoapz.png",
-		title: "VACB&",
-		credit: "Rav-T",
-		trigger: SpecialEvents.hide
-	},
-	{
-		url: "https://i.imgur.com/bBpVw2u.gif",
-		title: "Corgi Loadin",
-		credit: "Rav-T",
-		trigger: SpecialEvents.loading
-	},
-	{
-		url: "http://i.imgur.com/BAfpoOo.gif",
-		title: "The furry cometh",
-		credit: "Rav-T",
-		trigger: SpecialEvents.loading
-	},
-	{
-		url: "http://i.imgur.com/KwlQODl.jpg",
-		title: "WHYFIRE",
-		credit: "Brett"
-	},
-	{
-		url: "http://i.imgur.com/DvWDePs.png",
-		title: "Virtual Betrayal",
-		credit: "Matt"
-	},
-	{
-		url: "http://i.imgur.com/IYKrQm8.png",
-		title: "Owlturd",
-		credit: "Rav-T"
-	},
-	{
-		url: "http://i.imgur.com/XZmYKvQ.png",
-		title: "'Hot'",
-		credit: "Rav-T"
-	},
-  {
-    url: "",
-    title: "spooky",
-    credit: "Rav-T",
-    trigger: SpecialEvents.spooky
-    },
-  {
-    url: "https://i.imgur.com/VfkZWw6.png",
-    title: "buttsy",
-    credit: "Brett, Rav-T"
-  },
-  {
-    url: "https://imgur.com/5gQwuh8.png",
-    title: "Snek wit hat",
-    credit: "cards against humanity"
-  },
-  {
-    url:"https://i.imgur.com/GtvYSKc.jpg",
-    title: "Just Monika",
-    credit: "Mike"
-  },
-  {
-    url:"https://i.imgur.com/FP1VMYO.png",
-    title: "Danke Memes",
-    credit: "Rav-T"
-  },
-  {
-    url: "https://i.imgur.com/g0jiA9E.png",
-    title: "No grils",
-    credit: "Rav-T"
-  },
-  {
-    url: "https://i.imgur.com/aHFnCvV.png",
-    title: "later virgins",
-    credit: "Rav-T"
-  },
-  {
-    url:"",
-    title: "GGG CAH",
-    credit: "Rav-T's 1337 coding",
-    trigger: SpecialEvents.loadCards
-
-  }
+    }, {
+        url: "http://i.imgur.com/XUVcFQx.jpg",
+        title: "ERROR",
+        credit: "Ada",
+        trigger: SpecialEvents.indexError
+    }, {
+        url: "http://i.imgur.com/s65Yr86.png",
+        title: "components",
+        credit: "Ada, Ken"
+    }, {
+        url: "http://i.imgur.com/IEosSyF.png",
+        title: "SuperNERD",
+        credit: "Ada, Ken"
+    }, {
+        url: "http://i.imgur.com/CtRAxUR.png",
+        title: "Stranger Salts",
+        credit: "Ada"
+    }, {
+        url: "http://i.imgur.com/5qeoapz.png",
+        title: "VACB&",
+        credit: "Ada",
+        trigger: SpecialEvents.hide
+    }, {
+        url: "https://i.imgur.com/bBpVw2u.gif",
+        title: "Corgi Loadin",
+        credit: "Ada",
+        trigger: SpecialEvents.loading
+    }, {
+        url: "http://i.imgur.com/BAfpoOo.gif",
+        title: "The furry cometh",
+        credit: "Ada",
+        trigger: SpecialEvents.loading
+    }, {
+        url: "http://i.imgur.com/KwlQODl.jpg",
+        title: "WHYFIRE",
+        credit: "Brett"
+    }, {
+        url: "http://i.imgur.com/DvWDePs.png",
+        title: "Virtual Betrayal",
+        credit: "Matt"
+    }, {
+        url: "http://i.imgur.com/IYKrQm8.png",
+        title: "Owlturd",
+        credit: "Ada"
+    }, {
+        url: "http://i.imgur.com/XZmYKvQ.png",
+        title: "'Hot'",
+        credit: "Ada"
+    }, {
+        url: "",
+        title: "spooky",
+        credit: "Ada",
+        trigger: SpecialEvents.spooky
+    }, {
+        url: "https://i.imgur.com/VfkZWw6.png",
+        title: "buttsy",
+        credit: "Brett, Ada"
+    }, {
+        url: "https://imgur.com/5gQwuh8.png",
+        title: "Snek wit hat",
+        credit: "cards against humanity"
+    }, {
+        url: "https://i.imgur.com/GtvYSKc.jpg",
+        title: "Just Monika",
+        credit: "Mike"
+    }, {
+        url: "https://i.imgur.com/FP1VMYO.png",
+        title: "Danke Memes",
+        credit: "Ada"
+    }, {
+        url: "https://i.imgur.com/g0jiA9E.png",
+        title: "No grils",
+        credit: "Ada"
+    }, {
+        url: "https://i.imgur.com/aHFnCvV.png",
+        title: "later virgins",
+        credit: "Ada"
+    }, {
+        url: "",
+        title: "GGG CAH",
+        credit: "Ada's 1337 coding",
+        trigger: SpecialEvents.loadCards
+    }
 ];
