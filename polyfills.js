fa22de8e94a110f197acda99c3a2fdbeb9680674
ada <hiddenkrypt@gmail.com>
2021-06-16 09:58:35 -0700

    added HTMLSelectElement.remove polyfill, since that's also missing from chromium 18

--- polyfills.js
+++ polyfills.js
@@ -1,4 +1,20 @@
 
+/*HTMLSelectElement.remove polyfill by chenzhenxi on github: https://github.com/chenzhenxi/element-remove*/
+(function (arr) {
+    arr.forEach(function (item) {
+        if (item.hasOwnProperty('remove')) {
+            return;
+        }
+        Object.defineProperty(item, 'remove', {
+            configurable: true,
+            enumerable: true,
+            writable: true,
+            value: function remove() {
+                this.parentNode && this.parentNode.removeChild(this);
+            }
+        });
+    });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype].filter(Boolean));
 
 /*Array.findIndex polyfill*/
 if (!Array.prototype.findIndex) {
